45bf676a35c0ca0c6cb6824e13ae1d218158c0e7
bharadwaja2711 <110170133+bharadwaja2711@users.noreply.github.com>
2024-03-25 10:49:13 +0530

    Add files via upload
    Problem 1

---
+++ MergeSortedArray_1.java
@@ -0,0 +1,39 @@
+import java.util.*;
+class MergeSortedArray_1
+{
+    static void merge(int[] nums1, int m, int[] nums2, int n)
+    {
+        int sol[] = new int[m+n];
+        int i = 0, j = 0, k = 0;
+        while(i < m && j < n)
+        {
+            if(nums1[i] <= nums2[j])
+            {
+                sol[k] = nums1[i];
+                i++;
+            }
+            else
+            {
+                sol[k] = nums2[j];
+                j++;
+            }
+            k++;
+        }
+        while(i < m)
+        {
+            sol[k] = nums1[i];
+            i++;
+            k++;
+        }
+        while(j < n)
+        {
+            sol[k] = nums2[j];
+            j++;
+            k++;
+        }
+        for(int l = 0; l < m+n; l++)
+        {
+            nums1[l] = sol[l];
+        }
+    }
+}
